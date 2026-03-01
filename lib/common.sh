970c845a002d6432b8fc3380c0f702f31aad492f
adnanhd <adnanharundogan@gmail.com>
2026-03-02 00:53:55 +0300

    Fix color escape codes: use $'...' instead of '...' for ANSI sequences
    Single-quoted '\033[...' are literal strings, not escape sequences.
    This caused raw escape codes in heredocs and cat <<EOF output.

--- lib/common.sh
+++ lib/common.sh
@@ -3,13 +3,13 @@
 
 # Colors (only when stdout is a terminal)
 if [ -t 1 ]; then
-  RED='\033[0;31m'
-  GREEN='\033[0;32m'
-  YELLOW='\033[0;33m'
-  BLUE='\033[0;34m'
-  CYAN='\033[0;36m'
-  BOLD='\033[1m'
-  RESET='\033[0m'
+  RED=$'\033[0;31m'
+  GREEN=$'\033[0;32m'
+  YELLOW=$'\033[0;33m'
+  BLUE=$'\033[0;34m'
+  CYAN=$'\033[0;36m'
+  BOLD=$'\033[1m'
+  RESET=$'\033[0m'
 else
   RED='' GREEN='' YELLOW='' BLUE='' CYAN='' BOLD='' RESET=''
 fi
